195b154127c730c19cdf0e8f64d63713b7c8d700
grincode <juanpetarcich@gmail.com>
2024-03-03 13:02:01 -0300

    Paso 4 fixed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,80 +1,54 @@
 import { useState } from 'react'
 
+const Statistics = ({text, value}) => {
 
-const Statistics = ({ text, value }) => {
-  return (
+ return (
     <div>
       <p>{text} {value}</p>
     </div>
-  )
-}
+    )
+};
 
-const Button = (props) => (
-
-  <button
-    style={{
-      marginRight: '5px',
-      backgroundColor: 'black',
-      color: 'white',
-      border: 'none',
-      padding: '10px 20px',
-      borderRadius: '10px',
-      cursor: 'pointer',
-      transition: 'background-color 0.3s, color 0.3s',
-    }}
-    onClick={props.handleClick}
-    onMouseEnter={(e) => e.target.style.backgroundColor = 'red'}
-    onMouseLeave={(e) => e.target.style.backgroundColor = 'black'}
-  >
-    {props.text}
-  </button>
-
-)
-const Total = ({ good, neutral, bad }) => {
+const Total = ({good, neutral, bad}) => {
 
   const total = good + neutral + bad;
-  if (total === 0)
-    return (
-      <div>
-        No feedback given
-      </div>
+  if(total === 0){
+    return <div>No feedback given</div>    
+  }
+
+  return (
+    <div>
+      <Statistics text="good" value={good}/>
+      <Statistics text="neutral" value={neutral}/>
+      <Statistics text="bad" value={bad}/>
+      <Statistics text="average" value={((good - bad) / total).toFixed(2)}/>
+      <Statistics text="positive" value={((good / total) * 100).toFixed(2)}/>
+    </div>
     )
+};
 
-  return (
+
+  const App = () => {
+    // guarda los clics de cada botón en su propio estado
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodButton = () => setGood(good + 1)
+    const handleNeutralButton = () => setNeutral(neutral + 1)
+    const handleBadButton = () => setBad(bad + 1)
+
+    return (
     <div>
-      <Statistics text="good:" value={good} />
-      <Statistics text="neutral:" value={neutral} />
-      <Statistics text="bad:" value={bad} />
-      <hr width="25%" align="left" />
-
-      <Statistics text="Total:" value={total} />
-      <Statistics text="Average:" value={((good - bad) / total).toFixed(2)} />
-      <Statistics text="Positive:" value={((good / total) * 100).toFixed(2)} />
-
-
-    </div>
-  )
-}
-
-
-const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      <Header name="Give feedback" />
-    </div>
-  )
-    < div >
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={handleGoodButton}>good</button>
+        <button onClick={handleNeutralButton}>neutral</button>
+        <button onClick={handleBadButton}>bad</button>        
+      </div>
       <h2>Statistics</h2>
-      <Total good={good} neutral={neutral} bad={bad} />
-    </div >
+      <Total good={good} neutral={neutral} bad={bad} />      
+    </div>
   )
 }
 
